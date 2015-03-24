1a8b3a42db4166b3dd08ebe9f02f6f0b4743d6c3
Vale <valetolpegin@gmail.com>
2015-03-24 17:06:09 -0400

    Finished graphical interface

--- Scoring_App/app/src/main/java/com/gmail/valetolpegin/scoring_app/DriverControlledOptions.java
+++ Scoring_App/app/src/main/java/com/gmail/valetolpegin/scoring_app/DriverControlledOptions.java
@@ -6,18 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AutonomousOptions extends Fragment {
-
+public class DriverControlledOptions extends Fragment {
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        getActivity().setTitle(R.string.app_name);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View v = inflater.inflate( R.layout.fragment_autonomous_options, parent, false );
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
+    {
+        View v = inflater.inflate( R.layout.fragment_driver_controlled_options, parent, false );
 
         return v;
     }
--- Scoring_App/app/src/main/java/com/gmail/valetolpegin/scoring_app/MainActivity.java
+++ Scoring_App/app/src/main/java/com/gmail/valetolpegin/scoring_app/MainActivity.java
@@ -2,12 +2,11 @@ package com.gmail.valetolpegin.scoring_app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
 
 public class MainActivity extends ActionBarActivity {
     private android.app.Fragment mAutonomousFragment;
+    private android.app.Fragment mDriverControlledFragment;
+    private android.app.Fragment mMainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,38 +14,15 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         android.app.FragmentManager fm = getFragmentManager();
-        mAutonomousFragment = fm.findFragmentById( R.id.fragmentContainer );
+        mMainFragment = fm.findFragmentById( R.id.fragmentContainer );
 
-        if ( mAutonomousFragment == null )
+        if ( mMainFragment == null )
         {
-            mAutonomousFragment = new AutonomousOptions();
+            mMainFragment = new MainFragment();
 
             fm.beginTransaction()
-                    .add( R.id.fragmentContainer, mAutonomousFragment )
+                    .add( R.id.fragmentContainer, mMainFragment )
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
